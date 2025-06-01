e322105fa74fe04430b8f7b32d84c11fc4f6c1c3
Lady-Marianne <mariana.andrea.lois@gmail.com>
2025-06-01 15:51:56 -0300

    Database created succesfully.

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import os
 
 # Configure the Flask app and database:
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/jubigestion.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database', 'jubigestion.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
