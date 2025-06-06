33f025700b4bb0f6c9342e7818eb215579dae447
Lady-Marianne <mariana.andrea.lois@gmail.com>
2025-06-06 04:35:18 -0300

    Change 'database_setup.py' to make it create the database empty.

--- database_setup.py
+++ database_setup.py
@@ -11,14 +11,15 @@ if not os.path.exists("database"):
 else:
     print ("La carpeta 'database' ya existe.")
 
-# Create the database (if it doesn't exist) and the tables:
+# Create the database (if it doesn't exist) but NOT the tables:
 def create_database(app):
     db_path = 'database/jubigestion.db'
     print(f"Ruta de la base de datos: {db_path}")
     print(f"¿Existe la base? {os.path.exists(db_path)}")
     if not os.path.exists(db_path):
-        with app.app_context():
-            db.create_all()
-            print("Base de datos creada exitosamente.")
+        # Solo crear archivo vacío de base de datos:
+        open(db_path, 'a').close()
+        print("Base de datos creada exitosamente (vacía, sin tablas).")
     else:
-        print("La base de datos ya existe.")        
+        print("La base de datos ya existe.")
+        
