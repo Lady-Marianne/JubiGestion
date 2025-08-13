63f9446ff48967b9ed6a0d1c9148a84dd1d3c31f
Lady-Marianne <mariana.andrea.lois@gmail.com>
2025-08-13 10:21:36 -0300

    Change base_model.

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,8 @@ from datetime import date, datetime
 class BaseModel(db.Model):
     __abstract__ = True  # A table is not created for this class, it serves as a base for other models.
 
-    def to_dict(self):
+    def to_dict(self, includeRelationships=False):
+
         result = {}
         for column in self.__table__.columns:
             value = getattr(self, column.name)
@@ -15,12 +16,20 @@ class BaseModel(db.Model):
             # Convert enums to their value (For example: Gender.M -> 'M')
             if isinstance(value, Enum):
                 result[column.name] = value.value
-
             # Convert dates to string:
             elif isinstance(value, (date, datetime)):
                 result[column.name] = value.isoformat()
-
             else:
                 result[column.name] = value
 
+        if includeRelationships:
+            for rel in self.__mapper__.relationships:
+                related_value = getattr(self, rel.key)
+                if related_value is None:
+                    result[rel.key] = None
+                elif isinstance(related_value, list):
+                    result[rel.key] = [item.to_dict() for item in related_value]
+                else:
+                    result[rel.key] = related_value.to_dict()
+                    
         return result
